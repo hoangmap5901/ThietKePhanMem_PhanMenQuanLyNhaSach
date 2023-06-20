4d9c363c84f376ddcc8fc880b1d9078de19361b9
Nguyen Duc Hoang <cunmap5901@gmail.com>
2023-06-20 21:18:48 +0700

    Saved finalized draft 4

--- MyShop/obj/Debug/net5.0-windows/ThayDoiQuyDinhWindow.g.i.cs
+++ MyShop/obj/Debug/net5.0-windows/ThayDoiQuyDinhWindow.g.i.cs
@@ -93,7 +93,7 @@ namespace MyShop {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MyShop;component/thaydoiquydinhwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MyShop;V1.0.0.0;component/thaydoiquydinhwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ThayDoiQuyDinhWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
